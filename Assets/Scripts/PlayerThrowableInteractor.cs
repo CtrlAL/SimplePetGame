b04e2de67705add51de68b287ecb0325bc5e237c
unknown <sasha.lartsev@gmail.com>
2025-06-25 15:48:43 +0400

    Добавил возмонжость положить предмет

--- Assets/Scripts/PlayerThrowableInteractor.cs
+++ Assets/Scripts/PlayerThrowableInteractor.cs
@@ -9,15 +9,31 @@ public class PlayerThrowableInteractor : MonoBehaviour
     [SerializeField]
     private GameObject _throwablesSlot;
 
+    [SerializeField]
+    private GameObject _putSlot;
+
     private List<GameObject> _allowThrowables;
 
     private GameObject _pickedObject;
 
     public void Awake()
     {
-        PlayerInputProvider.GetInputActions().Inputs.Pickup.performed += Pickup;
+        PlayerInputProvider.GetInputActions().Inputs.Pickup.performed += PickOrPut;
         _allowThrowables = new List<GameObject>();
     }
+
+    private void PickOrPut(InputAction.CallbackContext context)
+    {
+        if (_pickedObject == null)
+        {
+            Pickup(context);
+        }
+        else
+        {
+            Put(context);
+        }
+    }
+
 
     private void Pickup(InputAction.CallbackContext context)
     {
@@ -30,20 +46,34 @@ public class PlayerThrowableInteractor : MonoBehaviour
             .OrderBy(go => Vector3.Distance(player.transform.position, go.transform.position))
             .FirstOrDefault();
 
-        if (closestThrowable != null)
+        if (closestThrowable != null && closestThrowable.TryGetComponent<Rigidbody>(out var rb))
         {
             closestThrowable.transform.position = _throwablesSlot.transform.position;
             closestThrowable.transform.SetParent(_throwablesSlot.transform);
+            rb.useGravity = false;
+            rb.isKinematic = true;
             _pickedObject = closestThrowable;
         }
     }
 
     private void Put(InputAction.CallbackContext context)
     {
+        if (_pickedObject != null && _pickedObject.TryGetComponent<Rigidbody>(out var rb))
+        {
+            _pickedObject.transform.position = _putSlot.transform.position;
+            _pickedObject.transform.SetParent(null);
+            rb.useGravity = true;
+            rb.isKinematic = false;
+            _pickedObject = null;
+        }
     }
 
     private void Throw(InputAction.CallbackContext context)
     {
+        if (_pickedObject != null)
+        {
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
