94c34511a70f04ea949a5cb097a969ab64d75d6e
unknown <sasha.lartsev@gmail.com>
2025-06-25 19:13:30 +0400

    Стан работает но он плохой

---
+++ Assets/CollisionDetector.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject _stunnedIcon;
+
+    [SerializeField]
+    private int _maxHitCount = 0;
+
+    private int _currentHitCount = 0;
+
+    private bool _isStuned = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_isStuned)
+        {
+            if (collision.gameObject.CompareTag("Environment"))
+            {
+                StartCoroutine(ObjectStuned());
+            }
+
+            else if (collision.gameObject.CompareTag("Throwable"))
+            {
+                _currentHitCount++;
+
+                if (_currentHitCount == _maxHitCount)
+                {
+                    StartCoroutine(ObjectStuned());
+                }
+            }
+        }
+    }
+
+    private IEnumerator ObjectStuned()
+    {
+        _isStuned = true;
+        _currentHitCount = 0;
+
+        var rb = gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.isKinematic = true;
+            _stunnedIcon.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(10f);
+
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+
+        _stunnedIcon.SetActive(false);
+        _isStuned = false;
+    }
+}
