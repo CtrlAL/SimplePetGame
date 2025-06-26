c67d58cadf36cb2bfc7df7be8a754c230b1b3b1f
unknown <sasha.lartsev@gmail.com>
2025-06-27 00:11:10 +0400

    Сделал по читаемее

--- Assets/Scripts/EnemyInput.cs
+++ Assets/Scripts/EnemyInput.cs
@@ -5,8 +5,11 @@ namespace Assets.Scripts
 {
     public class EnemyInput : MonoBehaviour
     {
-        [SerializeField] NavMeshAgent _agent;
-        [SerializeField] Rigidbody _rigidbody;
+        [SerializeField] 
+        private NavMeshAgent _agent;
+
+        [SerializeField] 
+        private Rigidbody _rigidbody;
 
         public void Awake()
         {
@@ -14,18 +17,25 @@ namespace Assets.Scripts
             _agent.updateRotation = true;
         }
 
-
         public void FixedUpdate()
         {
             if (PlayerInstanse.Instance != null && _agent != null && _rigidbody != null)
             {
-                var target = PlayerInstanse.Instance.transform.position;
-                _agent.SetDestination(target);
-                var desiredVelocity = _agent.desiredVelocity;
-
-                _rigidbody.MovePosition(_rigidbody.position + desiredVelocity * Time.fixedDeltaTime);
-                _agent.nextPosition = transform.position;
+                AIUpdatePosition();
             }
+        }
+
+        private void AIUpdatePosition()
+        {
+            var target = PlayerInstanse.Instance.transform.position;
+
+            _agent.SetDestination(target);
+
+            var desiredVelocity = _agent.desiredVelocity;
+
+            _rigidbody.MovePosition(_rigidbody.position + desiredVelocity * Time.fixedDeltaTime);
+
+            _agent.nextPosition = transform.position;
         }
     }
 }
