1618902dbc2b94087a4d312838704a5820f2d3d7
CtrlAL <sasha.lartsev@gmail.com>
2025-06-29 00:15:39 +0400

    Переименовал переменную в фатиге

--- Assets/Scripts/Fatigue.cs
+++ Assets/Scripts/Fatigue.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class Fatigue : MonoBehaviour
 {
     [SerializeField]
-    private float _fatigueThrashold = 100f;
+    private float _fatigueThreshold = 100f;
 
-    public float FatigueThrashold => _fatigueThrashold;
+    public float FatigueThreshold => _fatigueThreshold;
 
     public float CurrentFatigue { get; private set; } = 0f;
 
     public void MakeFatigueDamake(int damage)
     {
-        if (CurrentFatigue + damage < _fatigueThrashold)
+        if (CurrentFatigue + damage < _fatigueThreshold)
         {
             CurrentFatigue += damage;
         }
@@ -20,5 +20,14 @@ public class Fatigue : MonoBehaviour
     public void Reset(int damage)
     {
         CurrentFatigue = 0f;
+    }
+
+    public float GetKnockbackMultiplier(float maxMultiplier = 2f)
+    {
+        if (_fatigueThreshold <= 0f) return 1f;
+
+        float fatigueRatio = Mathf.Clamp01(CurrentFatigue / _fatigueThreshold);
+
+        return 1f + (fatigueRatio * (maxMultiplier - 1f));
     }
 }
