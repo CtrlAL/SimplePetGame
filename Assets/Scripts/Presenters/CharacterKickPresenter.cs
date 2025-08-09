2fd136b3bf7606f3f4b55853bae3ef85b2050d1e
unknown <sasha.lartsev@gmail.com>
2025-08-09 20:42:25 +0400

    Стан почему то на персонааж применяется

--- Assets/Scripts/Presenters/CharacterKickPresenter.cs
+++ Assets/Scripts/Presenters/CharacterKickPresenter.cs
@@ -2,5 +2,6 @@
 {
     public class CharacterKickPresenter
     {
+        // Всю фигню со станом сюда, сервис и подписка на ивенты
     }
 }
--- Assets/Scripts/Presenters/CharacterStunPresenter.cs
+++ Assets/Scripts/Presenters/CharacterStunPresenter.cs
@@ -6,5 +6,6 @@ namespace Presenters
 {
     public class CharacterStunPresenter
     {
+        // Всю фигню со станом сюда, сервис и подписка на ивенты
     }
 }
