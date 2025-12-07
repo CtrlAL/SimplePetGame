45cdf2731b008e72c788ffc291aa3ebc310aa684
unknown <sasha.lartsev@gmail.com>
2025-12-07 23:47:50 +0400

    Fix enemy spawner

--- Assets/Scripts/Presenters/SpawnEnemyPresenter.cs
+++ Assets/Scripts/Presenters/SpawnEnemyPresenter.cs
@@ -1,18 +1,42 @@
 using ScriptableObjects;
 using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
+using Views;
 using Zenject;
 
 namespace Presenters
 {
 
-    public class SpawnEnemyPresenter : IFixedTickable
+    public class SpawnEnemyPresenter : IFixedTickable, IInitializable, IDisposable
     {
         [Inject] private IEnemyFactory _enemyFactory;
         [Inject] private LevelSettings _levelSettings;
+        [Inject] private List<RespawnColiderView> _respawnColiderViews;
 
         private float _timer = 0f;
         private float _currentCount = 0f;
+        private CompositeDisposable _compositeDisposable = new();
+
+        public void Initialize()
+        {
+            _respawnColiderViews.ForEach(x =>
+            {
+                x.OnCharacterFell
+                .Subscribe(
+                    co =>
+                    {
+                        if (co.CompareTag("Enemy"))
+                        {
+                            _currentCount--;
+                        }
+                    }
+                )
+                .AddTo(_compositeDisposable);
+            });
+        }
 
         public void FixedTick()
         {
@@ -24,6 +48,11 @@ namespace Presenters
                 _currentCount++;
                 _timer = 0f;
             }
+        }
+
+        public void Dispose()
+        {
+            _compositeDisposable?.Dispose();
         }
     }
 }
