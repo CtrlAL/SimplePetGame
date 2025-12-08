b8cab87ad6b226a46ef6f12566ee1f06323ff832
unknown <sasha.lartsev@gmail.com>
2025-12-08 23:21:54 +0400

    Remove material bind from installer

--- Assets/Scripts/Installers/Character/CharacterInstaller.cs
+++ Assets/Scripts/Installers/Character/CharacterInstaller.cs
@@ -13,7 +13,6 @@ namespace Services.Installers
     {
         public override void InstallBindings()
         {
-            Container.Bind<Material>().FromComponentOnRoot().AsCached();
             Container.Bind<Transform>().FromComponentOnRoot().AsCached();
             Container.Bind<BoxCollider>().FromComponentOnRoot().AsCached();
             Container.Bind<Rigidbody>().FromComponentOnRoot().AsSingle();
